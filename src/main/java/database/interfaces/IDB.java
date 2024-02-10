4c368360af4f733299c069bbcf1e5288b94a44f1
Nurassyl <153646406+nurikesh@users.noreply.github.com>
2024-02-10 23:26:15 +0600

    // BetterComments!
    Improved comments in the whole project.

--- src/main/java/database/interfaces/IDB.java
+++ src/main/java/database/interfaces/IDB.java
@@ -2,8 +2,8 @@ package database.interfaces;
 
 import java.sql.Connection;
 
-// Interface defining methods for obtaining a database connection
+// Interface defining methods for obtaining a database connection.
 public interface IDB {
-    // Method signature for obtaining a connection to the database
+    // Method signature for obtaining a connection to the database.
     Connection getConnection();
 }
