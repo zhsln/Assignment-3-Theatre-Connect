4e798eb30c38fc5db8751e57ba7c8a6507b5d622
Nurassyl <153646406+nurikesh@users.noreply.github.com>
2024-02-24 21:17:27 +0600

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
 import consoleManager.ConsoleMainManager;
-import controllers.AdminController;
 import controllers.BookingController;
 import controllers.PerformanceController;
 import controllers.UserController;
 import database.DatabaseConnection;
 import database.interfaces.IDB;
-import repositories.AdminRepository;
 import repositories.BookingRepository;
 import repositories.PerformanceRepository;
 import repositories.UserRepository;
@@ -16,21 +14,20 @@ public class Main {
         // Declaring database connection interface to connect with database.
         IDB db = new DatabaseConnection();
 
-        // Declaring each repositories to work with each of them by controllers.
+        // Declaring each repository to work with each of them by controllers.
         UserRepository userRepository = new UserRepository(db);
-        AdminRepository adminRepository = new AdminRepository(db);
         PerformanceRepository performanceRepository = new PerformanceRepository(db);
         BookingRepository bookingRepository = new BookingRepository(db);
 
         // Declaring controllers to work with repositories.
         UserController userController = new UserController(userRepository);
-        AdminController adminController = new AdminController(adminRepository);
         PerformanceController performanceController = new PerformanceController(performanceRepository);
         BookingController bookingController = new BookingController(bookingRepository);
 
         // Declaring main console manager.
-        ConsoleMainManager consoleMainManager = new ConsoleMainManager(userController, adminController,
-                                                                        performanceController, bookingController);
+        ConsoleMainManager consoleMainManager = new ConsoleMainManager(userController,
+                                                                        performanceController,
+                                                                        bookingController);
 
         consoleMainManager.startMainMenu();
     }
